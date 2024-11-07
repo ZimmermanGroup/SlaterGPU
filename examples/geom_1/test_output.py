c2cf043b73a73f7401607d5699d1172191d92c9a
joshkamm <joshkamm@umich.edu>
2024-11-07 03:27:20 -0500

    #11 flesh out pytest for geom_1 example
    add numpy to pixi pytest environment

--- examples/geom_1/test_output.py
+++ examples/geom_1/test_output.py
@@ -1,2 +1,18 @@
+import os
+from pathlib import Path
+import numpy as np
+import pytest
+
+
 def test_output():
-    assert True
+    # convert from script location to corresponding location in build directory
+    script_dir = Path(os.path.abspath(__file__)).parent
+    script_dir_parts = list(script_dir.parts)
+    script_dir_parts.insert(-2, "build")
+    script_build_dir = Path(*script_dir_parts)
+
+    assert script_build_dir.exists()
+    A = np.loadtxt(script_build_dir / "A", skiprows=1)
+    A_ref = np.loadtxt(script_build_dir / "A_ref", skiprows=1)
+    assert A.shape == A_ref.shape
+    assert A == pytest.approx(A_ref)
