707f4543269e899d793df525d889e8687a4b01a5
Paul Zimmerman <paulzim@umich.edu>
2024-11-12 15:45:40 -0500

    Update grid_util.cpp

--- src/integrals/grid_util.cpp
+++ src/integrals/grid_util.cpp
@@ -5,8 +5,7 @@
 
 using namespace std;
 
-//delete this file, these functions are in integrals_aux
-#if 0
+
 void get_angular_grid(int size_ang, double* ang_g, double* ang_w)
 {
   double* xa = new double[size_ang];
@@ -61,5 +60,3 @@ int electron_count(int charge, int natoms, int* atno, int& Nc, int& Na, int& Nb)
   return No;
 }
 
-
-#endif
