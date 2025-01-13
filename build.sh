17e22fe3250d12e1cc57a863a7106f271a10d76b
joshkamm <joshkamm@umich.edu>
2025-01-13 18:07:31 -0500

    update to pixi 0.40.0

--- build.sh
+++ build.sh
@@ -6,7 +6,8 @@ module load nvidia-sdk
 cmake --version
 
 # bash -i
-# sleep 10
-cmake -B $SRC_DIR/../build -S "/home/joshkamm/ElectronicStructure/SlaterGPU/"
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -B $SRC_DIR/../build -S "/home/joshkamm/ElectronicStructure/SlaterGPU/"
 cmake --build $SRC_DIR/../build -j 16
 
+
+sleep 90
