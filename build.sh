53047d98426a75c6986f81abf82c2589e760f1bf
joshkamm <joshkamm@umich.edu>
2025-01-14 04:01:50 -0500

    pixi build progress

--- build.sh
+++ build.sh
@@ -5,9 +5,15 @@ module load nvidia-sdk
 
 cmake --version
 
-# bash -i
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -B $SRC_DIR/../build -S "/home/joshkamm/ElectronicStructure/SlaterGPU/"
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -B $SRC_DIR/../build -S $RECIPE_DIR
 cmake --build $SRC_DIR/../build -j 16
 
+cp -r ../build/lib $PREFIX/lib
+mkdir $PREFIX/bin
+cp ../build/examples/sgpu.exe $PREFIX/bin
 
-sleep 90
+# touch WORKING
+# while [ -f "WORKING" ]; do
+#   sleep 1
+# done
+# echo "File 'WORKING' no longer exists! Continuing execution..."
