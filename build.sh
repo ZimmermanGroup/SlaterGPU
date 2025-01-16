de922bbb53d27c716fb46e206d7060009ce152ee
joshkamm <joshkamm@umich.edu>
2025-01-16 04:42:32 -0500

    clean up pixi.toml

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -B $SRC_DIR/../build -S $RECIPE_DIR
 cmake --build $SRC_DIR/../build -j 16
 
 cp -r ../build/lib $PREFIX/lib
-mkdir $PREFIX/bin
+mkdir -p $PREFIX/bin
 cp ../build/examples/sgpu.exe $PREFIX/bin
 
 # touch WORKING
