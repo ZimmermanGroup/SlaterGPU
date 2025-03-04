c6e34c07593f1befe2f060ed00bbccc6a0349ac6
joshkamm <joshkamm@umich.edu>
2025-03-04 00:53:16 -0500

    Streamline gh action NVHPC installation: download and extract in a single step

--- .github/workflows/install_nvhpc.sh
+++ .github/workflows/install_nvhpc.sh
@@ -2,8 +2,10 @@ pixi global install cmake
 pixi global install environment-modules
 pixi global install helix
 
-wget --progress=bar:force:noscroll https://developer.download.nvidia.com/hpc-sdk/25.1/nvhpc_2025_251_Linux_x86_64_cuda_12.6.tar.gz
-tar xpzf nvhpc_2025_251_Linux_x86_64_cuda_12.6.tar.gz
+# Stream download directly into tar extraction
+echo "Downloading and extracting NVHPC simultaneously..."
+wget -O - --progress=bar:force:noscroll https://developer.download.nvidia.com/hpc-sdk/25.1/nvhpc_2025_251_Linux_x86_64_cuda_12.6.tar.gz | tar xpz
+
 export NVHPC_SILENT="true"
 export NVHPC_INSTALL_DIR="/home/runner/work"
 export NVHPC_INSTALL_TYPE="auto"
