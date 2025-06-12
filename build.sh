12d517d66563ea1bcac736c38d8c192a82dcc649
joshkamm <joshkamm@umich.edu>
2025-06-12 04:58:01 -0400

    Enhance CMake configuration for installation and include directories

--- build.sh
+++ build.sh
@@ -4,10 +4,7 @@ cmake --version
 
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -B $SRC_DIR/../build -S $RECIPE_DIR
 cmake --build $SRC_DIR/../build -j 16
-
-cp -r ../build/lib/* $PREFIX/lib
-mkdir -p $PREFIX/bin
-cp ../build/examples/sgpu.exe $PREFIX/bin
+cmake --install $SRC_DIR/../build
 
 # touch WORKING
 # while [ -f "WORKING" ]; do
