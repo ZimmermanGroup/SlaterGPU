cf81bd046bf6eb5eea869f6ff54f3acf59f30b4a
Paul Zimmerman <paulzim@umich.edu>
2025-07-30 15:35:36 -0400

    gaussian integral functions
    Note: the three new function calls in main.cpp should be moved out of main and into a respectable location

--- examples/main.cpp
+++ examples/main.cpp
@@ -16,7 +16,202 @@
 #include "prosph.h"
 #include "quad.h"
 
+#include "../include/cintwrapper.h"
+#include "../include/cintprep.h"
+
 using namespace std;
+
+
+
+vector<vector<double> > setup_integrals_gsgpu(vector<vector<double> >& basis_aux, int natoms, int* atno, double* coords, int& nbas, int& nenv, int& N, int& Naux, int& nbas_ri, int* &atm, int* &bas, double* &env, int prl)
+{
+  string basfile = "basis";
+  string auxfile = "aux";
+
+  BT::DO_CART = false;
+  bool do_ri = true;
+  CINTPrep inp(do_ri);
+  //inp.read_xyz(xyzfile);
+  inp.assign_coords(natoms,atno,coords,true);
+  inp.read_bas(basfile);
+  bool found_ri = inp.read_bas_ri(auxfile);
+  if (!found_ri) inp.do_ri = false;
+
+  inp.prep_env();
+
+  nbas = inp.get_nbas();
+  nenv = inp.get_nenv();
+  N = inp.get_var_dim();
+  if (found_ri)
+  {
+    Naux = inp.get_var_dim_ri();
+    nbas_ri = inp.get_nbas_ri();
+  }
+  else
+    nbas_ri = Naux = 0;
+
+  if (prl>-1) printf("\n  setup_integrals_g. natoms: %2i nbas: %2i nenv: %2i N: %2i Naux: %2i nbas_ri: %2i \n",natoms,nbas,nenv,N,Naux,nbas_ri);
+
+  atm = inp.get_atm();
+  bas = inp.get_bas();
+  env = inp.get_env();
+
+  map< int, basis_t > basmap = inp.basmap;
+
+  vector<vector<double> > basis;
+  for (int n=0;n<natoms;n++)
+  {
+    int i1 = atno[n];
+    basis_t basis1 = basmap[i1];
+    int Z = basis1.nuc;
+    int nshell = basis1.shells.size();
+    int nbasat = inp.anum_to_N[Z];
+    if (prl>0)
+      printf("  atom %2i has atomic num %2i with %2i shells and %3i basis \n",n+1,Z,nshell,nbasat);
+
+    if (prl>2)
+    for (int j=0;j<nshell;j++)
+    {
+      int size1 = basis1.exps[j].size();
+      int l1 = basis1.shells[j];
+      printf("   shell %i has %2i Gaussians and l1: %i \n",j,size1,l1);
+      for (int k=0;k<size1;k++)
+        printf("   zeta:  %8.5f  norm:  %8.5f / %8.5f \n",basis1.exps[j][k],basis1.coef[j][k],get_gto_norm(l1,basis1.exps[j][k]));
+    }
+  }
+  printf("\n");
+
+  double n0 = norm_sh(0,0);
+  for (int n=0;n<natoms;n++)
+  {
+    int i1 = atno[n];
+    basis_t basis1 = basmap[i1];
+    int Z = basis1.nuc;
+    int nshell = basis1.shells.size();
+    //int nbasat = inp.anum_to_N[Z];
+
+    for (int j=0;j<nshell;j++)
+    {
+      int size1 = basis1.exps[j].size();
+      int l1 = basis1.shells[j];
+      int lmin = -l1; int lmax = l1;
+
+      for (int m=lmin;m<=lmax;m++)
+      {
+        vector<double> b1; for (int p=0;p<10;p++) b1.push_back(0);
+        b1[0] = l1+1; b1[1] = l1; b1[2] = m;
+       //using b1[3]==b1[4] to store # of gaussians
+        b1[3] = b1[4] = size1;
+        b1[5] = coords[3*n+0]; b1[6] = coords[3*n+1]; b1[7] = coords[3*n+2];
+        b1[8] = Z; b1[9] = n;
+
+       //zeta then normalization coeffs
+        for (int k=0;k<size1;k++)
+          b1.push_back(basis1.exps[j][k]);
+        for (int k=0;k<size1;k++)
+        {
+          double norm1 = get_gto_norm(l1,basis1.exps[j][k])*basis1.coef[j][k];
+          if (l1>0) norm1 *= norm_sh(l1,m)/n0;
+          b1.push_back(norm1);
+        }
+
+        basis.push_back(b1);
+      }
+    }
+  }
+
+  if (prl>1)
+  for (int i=0;i<basis.size();i++)
+  {
+    vector<double> basis1 = basis[i];
+    int n1 = basis1[0]; int l1 = basis1[1]; int m1 = basis1[2]; int ng = basis1[3];
+    double x1 = basis1[5]; double y1 = basis1[6]; double z1 = basis1[7]; double Z1 = basis1[8];
+    printf("  basis[%2i] nlm:  %i  %i %2i (XYZ: %8.5f %8.5f %8.5f Z: %3.2f) ng: %2i \n",i,n1,l1,m1,x1,y1,z1,Z1,ng);
+    for (int j=0;j<ng;j++)
+    {
+      printf("   %8.5f  %8.5f \n",basis1[10+j],basis1[10+ng+j]);
+    }
+  }
+
+ //just to make basis_aux's size correct
+  basis_aux.clear();
+  for (int i=0;i<Naux;i++)
+  {
+    vector<double> b1; b1.push_back(1);
+    basis_aux.push_back(b1);
+  }
+
+  return basis;
+}
+
+void compute_integrals_g(int natm, int nbas, int nenv, int N, int Naux, int nbas_ri, int* atm, int* bas, double* env, double* S, double* T, double* jH1, double* A, double* C, int prl)
+{
+  get_overlap(S, N, natm, nbas, nenv, atm, bas, env);
+  get_hcore(jH1, N, natm, nbas, nenv, atm, bas, env);
+  if (T!=NULL)
+    get_tcore(T, N, natm, nbas, nenv, atm, bas, env);
+
+  if (prl>1)
+  {
+    printf("\n S: \n");
+    print_square(N,S);
+    printf("\n jH1: \n");
+    print_square(N,jH1);
+  }
+
+  if (Naux>0)
+  {
+    gen_eri_2c(A, Naux, natm, nbas, nenv, nbas_ri, atm, bas, env);
+    gen_eri_3c(C, N, Naux, natm, nbas, nenv, nbas_ri, atm, bas, env);
+  }
+  else
+  {
+    printf("  no auxiliary basis \n");
+  }
+
+  if (prl>1)
+  {
+    printf("\n A: \n");
+    for (int m=0;m<Naux;m++)
+    {
+      for (int n=0;n<Naux;n++)
+        printf(" %8.5f",A[m*Naux+n]);
+      printf("\n");
+    }
+
+    int N2 = N*N;
+    printf("\n C: \n");
+    for (int m=0;m<N2;m++)
+    {
+      for (int n=0;n<Naux;n++)
+        printf(" %8.5f",C[m*Naux+n]);
+      printf("\n");
+    }
+  }
+
+  return;
+}
+void compute_gaussian_integrals_to_disk(int N, int Naux, int natoms, int nbas, int nenv, int nbas_ri, int* atm, int* bas, double* env)
+{
+  int N2 = N*N;
+  int Na2 = Naux*Naux;
+  int N2a = Naux*N2;
+
+  double* S = new double[N2]; double* jH1 = new double[N2]; double* A = new double[Na2]; double* C = new double[N2a];
+  double* T = new double[N2]; double* En = new double[N2];
+  for (int m=0;m<N2;m++) En[m] = 0.;
+
+  compute_integrals_g(natoms,nbas,nenv,N,Naux,nbas_ri,atm,bas,env,S,NULL,jH1,A,C,1);
+
+ //note we don't have separate En and T, so using sum of the two
+  for (int m=0;m<N2;m++) T[m] = jH1[m];
+  write_S_En_T(N,S,En,T);
+  write_square(Naux,A,"A",2);
+  write_C(Naux,N2,C);
+
+  return;
+}
+
 
 void compute_ps_integrals_to_disk(int natoms, int* atno, double* coords, vector<vector<double> > basis, vector<vector<double> > basis_aux, int prl)
 {
@@ -114,8 +309,10 @@ int main(int argc, char* argv[]) {
   vector< vector< double > > basis_aux;
   double Enn = 0;
 
+  bool gbasis = read_basis();
+
   // seting up
-  int natoms = initialize(false,basis,basis_aux,atno,coords,charge,nup,Enn,1);
+  int natoms = initialize(gbasis,basis,basis_aux,atno,coords,charge,nup,Enn,1);
   float * coordsf = new float[3*natoms];
   for (int i = 0; i < 3*natoms; i++) {
     coordsf[i] = coords[i];
@@ -129,6 +326,16 @@ int main(int argc, char* argv[]) {
 
   int N = basis.size();
   int Naux = basis_aux.size();
+
+  int nbas,nenv,nbas_ri;
+  int* atm; int* bas; double* env;
+  if (gbasis)
+  {
+    basis = setup_integrals_gsgpu(basis_aux,natoms,atno,coords,nbas,nenv,N,Naux,nbas_ri,atm,bas,env,1);
+
+    printf("  Gaussian basis sizes, N: %2i Naux: %3i \n",N,Naux);
+    printf("  basis size: %2i \n",basis.size());
+  }
 
   int N2 = N*N;
   int Naux2 = Naux*Naux;
@@ -177,7 +384,12 @@ int main(int argc, char* argv[]) {
     #pragma acc enter data create(g[0:N2*N2])
     printf("1e ints: %d\n2c2e ints: %d\n3c3e ints: %d\n4c4e ints: %d\n",N2, Naux2, N2a, N2*N2);
 
-    if (check_PS() > 0)
+    if (gbasis)
+    {
+      printf("\n TESTING gaussian integrals \n");
+      compute_gaussian_integrals_to_disk(N,Naux,natoms,nbas,nenv,nbas_ri,atm,bas,env);
+    }
+    else if (check_PS() > 0)
       compute_ps_integrals_to_disk(natoms,atno,coords,basis,basis_aux,prl);
     else
     {
